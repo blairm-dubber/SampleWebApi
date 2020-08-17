1053895ee4152fcce6adcc5b6699c11b0bce2341
Blair Morris <blair.morris@dubber.net>
2020-08-17 12:34:18 +1000

    Adding unit tests

---
+++ SampleWebApi.Test/SampleTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleWebApi.Test
+{
+    [TestClass]
+    public class SampleTests
+    {
+        [TestMethod]
+        public void ReturnsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
